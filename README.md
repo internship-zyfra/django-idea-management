5c936ac5a843a436e8a7b39b518ec34da07c013e
Andrey Knyazev <knyazev.andr99@gmail.com>
2023-08-06 23:21:29 +0300

    templates, static, media

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 docker-compose run --rm ideas sh -c "python3 manage.py <manage.py commands>"\
-runserver: docker-compose up
+runserver: docker-compose up\
+\
+Шаблоны кладем в idea_management/templates/<"django-application name">/
+
--- idea_management/idea_management/settings.py
+++ idea_management/idea_management/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third-party
+
+    # own
 ]
 
 MIDDLEWARE = [
@@ -53,7 +57,7 @@ ROOT_URLCONF = 'idea_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
