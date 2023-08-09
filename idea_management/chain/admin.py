5dc06f47fcb78bfd1b6983dccd5929ff0e0f30ba
Andrey Knyazev <knyazev.andr99@gmail.com>
2023-08-09 17:08:19 +0300

    admin fixed

--- idea_management/chain/admin.py
+++ idea_management/chain/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from chain.models import Chain, ChainLink, ChainLinkManager
 
-admin.site.register(Chain, ChainLink, ChainLinkManager)
+admin.site.register(Chain)
+admin.site.register(ChainLink)
+admin.site.register(ChainLinkManager)
