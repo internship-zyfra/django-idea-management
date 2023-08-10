b2da7ea735d741efdee735211d1745ed6b882499
Andrey Knyazev <knyazev.andr99@gmail.com>
2023-08-10 12:30:02 +0300

    fixes

--- idea_management/chain/models.py
+++ idea_management/chain/models.py
@@ -1,30 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.utils import timezone
-
-from accounts.models import User
 
 
 class Chain(models.Model):
-    chain_author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    chain_author_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
 
 
 class ChainLink(models.Model):
     stage_number = models.IntegerField()
-    chain_link = models.ForeignKey(Chain, on_delete=models.CASCADE)
+    chain_link = models.ForeignKey("chain.Chain", on_delete=models.CASCADE)
 
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
 
 
 class ChainLinkManager(models.Model):
-    manager_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    chain_link_id = models.ForeignKey(ChainLink, on_delete=models.CASCADE)
+    manager_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    chain_link_id = models.ForeignKey("chain.ChainLink", on_delete=models.CASCADE)
 
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
--- idea_management/idea/models.py
+++ idea_management/idea/models.py
@@ -1,37 +1,41 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.utils import timezone
-from tinymce.models import HTMLField
 
-from accounts.models import User
-from chain.models import Chain, ChainLink, ChainLinkManager
+from sorl.thumbnail import ImageField
+from tinymce.models import HTMLField
 
 
 class Idea(models.Model):
     title = models.CharField(max_length=150)
     body = HTMLField()
-    photo = models.ImageField(blank=True, upload_to='idea_image/')
-    chain_id = models.ForeignKey(Chain, on_delete=models.CASCADE)
-    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = ImageField(blank=True, null=True, upload_to='idea_image/')
+    chain_id = models.ForeignKey("chain.Chain", on_delete=models.CASCADE)
+    author_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     is_accepted = models.BooleanField(default=False)
     is_rejected = models.BooleanField(default=False)
-    current_chain_link = models.ForeignKey(ChainLink, blank=True, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(default=timezone.now)
+    current_chain_link = models.ForeignKey("chain.ChainLink", null=True, blank=True, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
 
 
 class IdeaMovement(models.Model):
+    ACCEPT = 1
+    REJECT = 2
     IDEA_MOVEMENT_TYPE = (
-        ('accept', 'Accept'),
-        ('reject', 'Reject'),
+        (ACCEPT, 'Accept'),
+        (REJECT, 'Reject'),
     )
 
-    type = models.CharField(choices=IDEA_MOVEMENT_TYPE, max_length=10)
-    manager_id = models.ForeignKey(ChainLinkManager, blank=True, on_delete=models.CASCADE)
+    type = models.PositiveSmallIntegerField(choices=IDEA_MOVEMENT_TYPE)
+    manager_id = models.ForeignKey("chain.ChainLinkManager", null=True, blank=True, on_delete=models.CASCADE)
     is_author_reject_his_idea = models.BooleanField(default=False)
-    comment = models.TextField(blank=True)
-    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
-    current_chain_link = models.ForeignKey(ChainLink, on_delete=models.CASCADE, related_name='current_idea_movements')
-    next_chain_link = models.ForeignKey(ChainLink, blank=True, on_delete=models.CASCADE,
+    comment = models.TextField(blank=True, null=True)
+    idea = models.ForeignKey("idea.Idea", on_delete=models.CASCADE)
+    current_chain_link = models.ForeignKey("chain.ChainLink", on_delete=models.CASCADE,
+                                           related_name='current_idea_movements')
+    next_chain_link = models.ForeignKey("chain.ChainLink", null=True, blank=True, on_delete=models.CASCADE,
                                         related_name='next_idea_movements')
-    created_at = models.DateTimeField(default=timezone.now)
+
+    created_at = models.DateTimeField(auto_now=True)
