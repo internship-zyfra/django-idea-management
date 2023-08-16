743362d29f817e8ba0132bedb9f202945499931a
Andrey Knyazev <Andrey Knyazev>
2023-08-16 18:59:57 +0300

    fix

--- idea_management/accounts/urls.py
+++ idea_management/accounts/urls.py
@@ -4,9 +4,9 @@ from accounts.views import SignInView, LogoutView, MainPageView, Page404View, Us
 app_name = 'accounts'
 
 urlpatterns = [
-    path("", MainPageView.as_view(), name='main'),
     path('login/', SignInView.as_view(), name='sign_in'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('page404/', Page404View.as_view(), name='page404'),
-    path("users/", UserListView.as_view(), name='users')
+    path("users/", UserListView.as_view(), name='users'),
+    path("", MainPageView.as_view(), name='main')
 ]
