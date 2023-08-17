61c1248dd73881ca066f5346c595298834771bf4
Arduinum-628 <message.chaos628@gmail.com>
2023-08-17 21:42:49 +0300

    added: create user functional, urls in navbar and main, unique for email

--- idea_management/accounts/urls.py
+++ idea_management/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from accounts.views import SignInView, LogoutView, MainPageView, Page404View
+from accounts.views import SignInView, LogoutView, MainPageView, Page404View, \
+CreateUserView
 
 
 app_name = 'accounts'
@@ -8,5 +9,6 @@ urlpatterns = [
     path('sign_in/', SignInView.as_view(), name='sign_in'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('page404/', Page404View.as_view(), name='page404'),
+    path('create_user/', CreateUserView.as_view(), name='create_user'),
     path("", MainPageView.as_view(), name='main')
 ]
